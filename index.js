54e7bb913774a0d21d36e872a4be78a69a516f1b
MaGo1024 <olumideodusote@gmail.com>
2019-07-02 19:38:19 +0100

    Global variable for PORT and Using Param

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ app.get('/api/courses', (req, res) => {   //API Endpoint
     res.send([1,2,3]);
 });
 
-// PORT
-const port = process.env.PORT || 3000;
-app.listen(3000, () => console.log(`Listening on port ${port}...`));
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+const port = process.env.PORT || 3000; // PORT
+app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 
 // Install Node Monitor to stop ending node app instances everytime with CRTL + Cnpm (npm i -g nodemon)
